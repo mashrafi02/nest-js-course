f654664f38454c56a14239e5f3dc4d54a6f2ddd2
Mashrafi <ahmedfirojmash365@gmail.com>
2026-02-10 12:57:56 +0600

    nest js course initialized

---
+++ 1-nest-js-intro/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MessageModule } from './message/message.module';
+
+@Module({
+  imports: [UsersModule, MessageModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
